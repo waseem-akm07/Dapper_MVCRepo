c8c25d93100dfc06657d9e7267993ab3592558ec
Waseem Akram <waseemakm07@gmail.com>
2022-09-17 12:31:52 +0530

    initial commit

---
+++ BusinessAcessLayer/Repo/BusinessLayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Configuration;
+using Dapper;
+using System.Data.SqlClient;
+using DataTransferObject.ViewModel;
+
+namespace BusinessAcessLayer.Repo
+{
+    public class BusinessLayer : IBusinessLayer
+    {
+        //public SqlConnection con;
+
+        //public void connection()
+        //{
+
+        //    string dbCon = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        //    con =new SqlConnection(dbCon);
+        //}
+
+
+        public IEnumerable<EmployeeModel> GetEmployees()
+        {
+
+            var sqlQuery = @"SELECT E.EmployeeId, E.EmployeeName, E.EmployeeSalary, D.DetailId, D.FatherName, D.Address, D.Contact, D.EmployeeId
+                             FROM Employee E INNER JOIN EmployeeDetail D ON E.EmployeeId = D.EmployeeId";
+
+
+            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ConnectionString))
+            {
+                return db.Query<EmployeeModel>(sqlQuery);
+            };
+        }
+
+        public IEnumerable<EmployeeModel> GetEmployeeById(int id)
+        {
+            var sqlQuery = @"SELECT E.EmployeeId, E.EmployeeName, E.EmployeeSalary, D.DetailId, D.FatherName, D.Address, D.Contact, D.EmployeeId
+                             FROM Employee E INNER JOIN EmployeeDetail D ON E.EmployeeId = D.EmployeeId WHERE E.EmployeeId =" + id;
+
+            using(IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ConnectionString))
+            {
+                return db.Query<EmployeeModel>(sqlQuery);
+            }
+               
+        }
+    }
+}
---
+++ BusinessAcessLayer/Repo/IBusinessLayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTransferObject.ViewModel;
+
+namespace BusinessAcessLayer.Repo
+{
+    public interface IBusinessLayer
+    {
+        IEnumerable<EmployeeModel> GetEmployees();
+        IEnumerable<EmployeeModel> GetEmployeeById(int id);
+    }
+}
